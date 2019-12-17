a87fb6cac00b4067a5bb734f301107fbe1af7361
Erik Norfelt <enorfelt@outlook.com>
2019-12-17 22:02:57 +0100

    longer toast

--- src/MobSwitcher.Windows/ToastService.cs
+++ src/MobSwitcher.Windows/ToastService.cs
@@ -1,5 +1,6 @@
 ﻿using MobSwitcher.Core.Services;
 using System;
+using Windows.Data.Xml.Dom;
 using Windows.UI.Notifications;
 
 namespace MobSwitcher.Windows
@@ -11,7 +12,7 @@ namespace MobSwitcher.Windows
             if (string.IsNullOrEmpty(message))
                 throw new ArgumentNullException(nameof(message));
 
-            var template = ToastNotificationManager.GetTemplateContent(ToastTemplateType.ToastText01);
+            var template = ToastNotificationManager.GetTemplateContent(ToastTemplateType.ToastImageAndText01);
 
             var textNodes = template.GetElementsByTagName("text");
             textNodes.Item(0).InnerText = message;
@@ -29,11 +30,31 @@ namespace MobSwitcher.Windows
             if (string.IsNullOrEmpty(message2))
                 throw new ArgumentNullException(nameof(message2));
 
-            var template = ToastNotificationManager.GetTemplateContent(ToastTemplateType.ToastText02);
+            var template = ToastNotificationManager.GetTemplateContent(ToastTemplateType.ToastImageAndText02);
+
+            var toastNode = template.SelectSingleNode("/toast") as XmlElement;
+            toastNode.SetAttribute("duration", "long");
+            
+
+            var audio = template.CreateElement("audio");
+            audio.SetAttribute("src", "ms-winsoundevent:Notification.Looping.Alarm");
+            audio.SetAttribute("loop", "true");
+            toastNode.AppendChild(audio);
 
             var textNodes = template.GetElementsByTagName("text");
             textNodes.Item(0).InnerText = message1;
             textNodes.Item(1).InnerText = message2;
+
+            var imageNodes = template.GetElementsByTagName("image");
+            if (imageNodes.Count > 0) 
+            {
+                var imageElement = imageNodes[0] as XmlElement;
+                if (imageElement != null) 
+                {
+                    imageElement.SetAttribute("src", "https://rawcdn.githack.com/enorfelt/MobSwitcher/ea626bc99df305d5263ffc063591bb9f3b3ffc08/icon.png");
+                    imageElement.SetAttribute("alt", "mobswitcher logo");
+                }
+            }
 
             var notifier = ToastNotificationManager.CreateToastNotifier("MobSwitcher");
             var notification = new ToastNotification(template);
