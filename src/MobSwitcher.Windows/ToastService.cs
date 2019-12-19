1b5b8448b2c058a99f36a66d05b605dbc6a1b4d7
Erik Norfelt <erik.norfelt@teleopti.com>
2019-12-19 08:41:23 +0100

    Changed toast to long and added image and sound

--- src/MobSwitcher.Windows/ToastService.cs
+++ src/MobSwitcher.Windows/ToastService.cs
@@ -1,5 +1,6 @@
 ﻿using MobSwitcher.Core.Services;
 using System;
+using System.IO;
 using Windows.Data.Xml.Dom;
 using Windows.UI.Notifications;
 
@@ -12,7 +13,7 @@ namespace MobSwitcher.Windows
             if (string.IsNullOrEmpty(message))
                 throw new ArgumentNullException(nameof(message));
 
-            var template = ToastNotificationManager.GetTemplateContent(ToastTemplateType.ToastImageAndText01);
+            var template = CreateToast(ToastTemplateType.ToastImageAndText01);
 
             var textNodes = template.GetElementsByTagName("text");
             textNodes.Item(0).InnerText = message;
@@ -30,35 +31,43 @@ namespace MobSwitcher.Windows
             if (string.IsNullOrEmpty(message2))
                 throw new ArgumentNullException(nameof(message2));
 
-            var template = ToastNotificationManager.GetTemplateContent(ToastTemplateType.ToastImageAndText02);
-
-            var toastNode = template.SelectSingleNode("/toast") as XmlElement;
-            toastNode.SetAttribute("duration", "long");
-            
-
-            var audio = template.CreateElement("audio");
-            audio.SetAttribute("src", "ms-winsoundevent:Notification.Looping.Alarm");
-            audio.SetAttribute("loop", "true");
-            toastNode.AppendChild(audio);
+            var template = CreateToast(ToastTemplateType.ToastImageAndText02);
 
             var textNodes = template.GetElementsByTagName("text");
             textNodes.Item(0).InnerText = message1;
             textNodes.Item(1).InnerText = message2;
+            
+            var notifier = ToastNotificationManager.CreateToastNotifier("MobSwitcher");
+            var notification = new ToastNotification(template);
+            notifier.Show(notification);
+        }
 
+        public static XmlDocument CreateToast(ToastTemplateType toastTemplateType)
+        {
+            var template = ToastNotificationManager.GetTemplateContent(toastTemplateType);
+
+            var toastNode = template.SelectSingleNode("/toast") as XmlElement;
+            toastNode.SetAttribute("duration", "long");
+
+
+            var audio = template.CreateElement("audio");
+            audio.SetAttribute("src", "ms-winsoundevent:Notification.Looping.Alarm4");
+            audio.SetAttribute("loop", "true");
+            toastNode.AppendChild(audio);
+            
             var imageNodes = template.GetElementsByTagName("image");
-            if (imageNodes.Count > 0) 
+            if (imageNodes.Count > 0)
             {
                 var imageElement = imageNodes[0] as XmlElement;
-                if (imageElement != null) 
+                if (imageElement != null)
                 {
-                    imageElement.SetAttribute("src", "https://rawcdn.githack.com/enorfelt/MobSwitcher/ea626bc99df305d5263ffc063591bb9f3b3ffc08/icon.png");
+                    var imagePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "toast-icon.png");
+                    imageElement.SetAttribute("src", imagePath);
                     imageElement.SetAttribute("alt", "mobswitcher logo");
                 }
             }
 
-            var notifier = ToastNotificationManager.CreateToastNotifier("MobSwitcher");
-            var notification = new ToastNotification(template);
-            notifier.Show(notification);
+            return template;
         }
     }
 }
