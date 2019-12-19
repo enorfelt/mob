20b8717802217e6e762849a76a93242da76918ab
Erik Norfelt <erik.norfelt@teleopti.com>
2019-12-19 09:04:23 +0100

    packaged 1.2.1

--- buildRelease.bat
+++ buildRelease.bat
@@ -2,3 +2,4 @@ dotnet restore
 dotnet build --no-restore
 dotnet test --no-restore
 dotnet publish --no-restore -c Release -o .\pkg\MobSwitcher\tools .\src\MobSwitcher.Cli\MobSwitcher.Cli.csproj
+checksum.exe .\pkg\MobSwitcher\tools\mob.exe -t=sha256
