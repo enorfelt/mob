5161686d355f772d8a099d6c011bca9c3fa5d9c5
Erik Norfelt <enorfelt@outlook.com>
2020-01-22 20:55:39 +0100

    choco msi installer

--- buildRelease.bat
+++ buildRelease.bat
@@ -2,4 +2,6 @@ dotnet restore
 dotnet build --no-restore
 dotnet test --no-restore
 dotnet publish --no-restore -c Release -o .\src\MobSwitcher.Installer .\src\MobSwitcher.Cli\MobSwitcher.Cli.csproj
-checksum.exe .\src\MobSwitcher.Installer\mob.exe -t=sha256
+candle.exe .\src\MobSwitcher.Installer\MobSwitcher-Installer.wxs -o .\src\MobSwitcher.Installer\MobSwitcher-Installer.wixobj
+light.exe -ext WixUIExtension .\src\MobSwitcher.Installer\MobSwitcher-Installer.wixobj -o .\pkg\MobSwitcher\tools\MobSwitcher-Installer.msi
+checksum.exe .\pkg\MobSwitcher\tools\MobSwitcher-Installer.msi -t=sha256
---
+++ pkg/MobSwitcher/tools/chocolateyinstall.ps1
@@ -0,0 +1,38 @@
+
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileLocation = Join-Path $toolsDir 'MobSwitcher-Installer.msi'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'MSI'
+  file         = $fileLocation
+  softwareName  = 'MobSwitcher*'
+  checksum      = 'CDF0227F7AFB1A58EDBC50AB1F943AC8840A8D0918A885C26B0A4084E03A69B6'
+  checksumType  = 'sha256'
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyInstallPackage @packageArgs
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
