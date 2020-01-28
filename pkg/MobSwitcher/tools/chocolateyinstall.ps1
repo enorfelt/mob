b7f119224f8b2d6c499e8480e7d4184a998b31e9
Erik Norfelt <erik.norfelt@teleopti.com>
2020-01-28 13:03:22 +0100

    show toast when installed to init toast app in windows

--- pkg/MobSwitcher/tools/chocolateyinstall.ps1
+++ pkg/MobSwitcher/tools/chocolateyinstall.ps1
@@ -1,22 +1,49 @@
 
 $ErrorActionPreference = 'Stop';
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $fileLocation = Join-Path $toolsDir 'MobSwitcher-Installer.msi'
 
+function Show-InstalledToast {
+  $ErrorActionPreference = "Stop"
+
+  $notificationTitle = "MobSwitcher installed!"
+
+  [Windows.UI.Notifications.ToastNotificationManager, Windows.UI.Notifications, ContentType = WindowsRuntime] > $null
+  $template = [Windows.UI.Notifications.ToastNotificationManager]::GetTemplateContent([Windows.UI.Notifications.ToastTemplateType]::ToastText01)
+
+  #Convert to .NET type for XML manipuration
+  $toastXml = [xml] $template.GetXml()
+  $toastXml.GetElementsByTagName("text").AppendChild($toastXml.CreateTextNode($notificationTitle)) > $null
+
+  #Convert back to WinRT type
+  $xml = New-Object Windows.Data.Xml.Dom.XmlDocument
+  $xml.LoadXml($toastXml.OuterXml)
+
+  $toast = [Windows.UI.Notifications.ToastNotification]::new($xml)
+  $toast.Tag = "mob-timer"
+  $toast.Group = "mobswitcher"
+  $toast.ExpirationTime = [DateTimeOffset]::Now.AddMinutes(5)
+  $toast.SuppressPopup = $true
+
+  $notifier = [Windows.UI.Notifications.ToastNotificationManager]::CreateToastNotifier("{fb4831aa-8a45-4828-ba0e-b840bcfb395a}!MobSwitcher")
+  $notifier.Show($toast);
+}
+
 $packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  unzipLocation = $toolsDir
-  fileType      = 'MSI'
-  file         = $fileLocation
-  softwareName  = 'MobSwitcher*'
-  checksum      = '3DBCA8ABF47616E5C6BBA900720C24AB1FDC848F7F787855EB85E3E4AB128336'
-  checksumType  = 'sha256'
-  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
-  validExitCodes= @(0, 3010, 1641)
+  packageName    = $env:ChocolateyPackageName
+  unzipLocation  = $toolsDir
+  fileType       = 'MSI'
+  file           = $fileLocation
+  softwareName   = 'MobSwitcher*'
+  checksum       = 'A3ADA51A52B529593B94754BD03BEB732037C25FDF696368B619AA9044F749BE'
+  checksumType   = 'sha256'
+  silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes = @(0, 3010, 1641)
 }
 
 Install-ChocolateyInstallPackage @packageArgs
-$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
+Show-InstalledToast
 
 
 
